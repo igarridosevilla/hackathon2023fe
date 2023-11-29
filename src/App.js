5b9cc2bfc4bcd1b4d47c0cf887d4bc4b4c15a507
izambrana <israel.zambrana@coverwallet.com>
2023-11-29 11:38:28 +0100

    test

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Create React App example
+          Material UI Create React App example 2
         </Typography>
         <ProTip />
         <Copyright />
