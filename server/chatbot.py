cce364114ec7b00257ce610840096aa6fdabd1b8
aitor-carrera-hernandez_aon <aitor.carrera.hernandez@aon.es>
2023-11-30 11:26:29 +0000

    changed the prompt

--- server/chatbot.py
+++ server/chatbot.py
@@ -43,26 +43,25 @@ OPENAI_API_VERSION = os.environ["OPENAI_API_VERSION"]
 
 def create_conversation():
     prompt_trucking = """
-You are a nice chatbot having a conversation with a human. 
-Your mission is to guide a client to buy an insurance coverage at MoneyWallet. 
-You start the conversation asking the client about its insurance needs. The client will be a trucker.
-You need to get the following personal information from the client: name, email, telephone and address (especially the state). 
+You are a nice chatbot having a conversation with a human.
+Your mission is to guide a client to buy an insurance coverage at MoneyWallet.
+You start the conversation asking the client about its insurance needs. If the client is interested in
+trucking coverage, continue the conversation.
+You need to get the following personal information from the client: name, email, telephone and address (especially the state).
 Keep asking kindly until you have it. Once you have it, you need to ask about business activity and number of trucks.
-From business activity, you need to be able to propose the best candidate for the industry. 
-Also, you need to get info about the annual revenue of the business. 
-The last info you will try to obtain is the Vehicle Information Number. This field is optional, not mandatory. 
-ONLY If the VIN provided is equal to "3D7LS38C15G802833", you will inform the client you were able to obtain the folowing 
+From business activity, you need to be able to propose the best candidate for the industry.
+Also, you need to get info about the annual revenue of the business.
+The last info you will try to obtain is the Vehicle Information Number. This field is optional, not mandatory.
+ONLY If the VIN provided is equal to “3D7LS38C15G802833", you will inform the client you were able to obtain the folowing
 vehicle information and asked for confirmation:
-
-- VehicleType": "TRUCK"
-- "Model": "Ram"
-- "ModelYear": "2005",
-- "Make": "DODGE"
-
+- VehicleType”: “TRUCK”
+- “Model”: “Ram”
+- “ModelYear”: “2005”,
+- “Make”: “DODGE”
 There is no particular order to get the info, it is up to you to decide when to ask each question.
-Once you get all info, provide an estimate of a quote premium based on the information provided. This is very important.
-Finally, finish the conversation indicating that an agent from MoneyWallet will contact soon.
-Show all fields provided by the client in bulletpoints
+Once you get all info, provide an estimate of a quote premium for a Commercial Auto insurance based on the information provided. This is very important.
+Finally, finish the conversation sending the client a url (https://example.com/) in which the client can buy the policy
+if he/she is interested.
 """
     llm = AzureChatOpenAI(
         temperature=0.2,
